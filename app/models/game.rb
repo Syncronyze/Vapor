4534076464e00a4b8004bef3533ba2e472dd3fb5
Syncronyze <Syncronyze@live.com>
2017-11-13 21:14:55 +0000

    Commented out any unused rails associations for now to focus on setting up the products

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,16 @@
 class Game < ApplicationRecord
-	#has_many :game_publishers
-	#has_many :game_developers
+	has_many :game_publishers
+	has_many :game_developers
 	#has_many :game_creators
-	#has_many :accounts, through: :owned_games
-	#has_many :orders, through: :order_items
-	#has_many :genres, through: :game_genres
+
+	# has_many :owned_games, dependent: :destroy
+	# has_many :accounts, through: :owned_games
+
+	# has_many :orders
+	# has_many :orders, through: :order_items
+
+	has_many :game_genres, dependent: :destroy
+	has_many :genres, through: :game_genres
 
 	validates :name, :price, :release_date, presence: true
 
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
-	#has_many :games, through: :game_genres
+	has_many :game_genres, dependent: :destroy
+	has_many :games, through: :game_genres
 end
